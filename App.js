01a0f9dd59e9c69202e1a7ed0767a8ec58cc7b53
ProgrammerJayt <theanthem8@gmail.com>
2025-04-24 08:36:48 +0200

    Wrap app with app context

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Navigator from "./src/navigation";
 import ToastManager from "toastify-react-native";
+import { AppProvider } from "./src/context/app";
 
 const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <Navigator />
-      <ToastManager />
+      <AppProvider>
+        <Navigator />
+        <ToastManager />
+      </AppProvider>
     </QueryClientProvider>
   );
 };
