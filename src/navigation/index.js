e370ab4593db56f0895767477a8b0d7922eb5e75
ProgrammerJayt <theanthem8@gmail.com>
2025-04-25 04:50:15 +0200

    Import custom header component

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,18 +1,22 @@
-// In App.js in a new project
-
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import * as React from "react";
+import ScreenHeaderComponent from "../components/ui/screen/header";
+import PostScreen from "../screens/post";
+import PostsScreen from "../screens/posts";
 import UserScreen from "../screens/user";
 import UsersScreen from "../screens/users";
-import PostsScreen from "../screens/posts";
-import PostScreen from "../screens/post";
 
 const Stack = createNativeStackNavigator();
 
 const RootStack = () => {
   return (
-    <Stack.Navigator initialRouteName="Users">
+    <Stack.Navigator
+      initialRouteName="Users"
+      screenOptions={({ navigation }) => ({
+        header: (props) => <ScreenHeaderComponent navigation={props} />,
+      })}
+    >
       <Stack.Screen name="User" component={UserScreen} />
       <Stack.Screen name="Users" component={UsersScreen} />
       <Stack.Screen name="Post" component={PostScreen} />
