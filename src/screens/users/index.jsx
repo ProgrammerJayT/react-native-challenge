db8686fffde9409cd667b6d83d58dc9c1f6f455b
ProgrammerJayt <theanthem8@gmail.com>
2025-04-25 04:51:27 +0200

    Push header buttons to custom header component

--- src/screens/users/index.jsx
+++ src/screens/users/index.jsx
@@ -1,14 +1,48 @@
-import React, { useEffect } from "react";
-import { SafeAreaView } from "react-native";
+import React, { useEffect, useLayoutEffect } from "react";
+import {
+  Alert,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import FlatListComponent from "../../components/flat-list";
 import UsersFlatListRenderComponent from "../../components/flat-list/users";
 import useReadUsers from "../../hooks/users";
+import { useNavigation } from "@react-navigation/native";
+import { useScreenHeaderContext } from "../../context/screen/header";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { useAppContext } from "../../context/app";
 
 const UsersScreen = () => {
-  const { data: users, isFetching:isReadUsersFetching } = useReadUsers({ enabled: true });
+  const navigator = useNavigation();
+  const { COLORS } = useAppContext();
+  const { setRightButtons } = useScreenHeaderContext();
+  const { data: users, isFetching: isReadUsersFetching } = useReadUsers({
+    enabled: true,
+  });
+
+  useLayoutEffect(() => {}, [navigator]);
+
+  const handlePostsClick = () => {
+    navigator.navigate("Posts");
+  };
 
   useEffect(() => {
-    // on mounted
+    setRightButtons([
+      {
+        page: "Users",
+        icon: (
+          <MaterialCommunityIcons
+            name="ballot-outline"
+            size={24}
+            color={COLORS.white}
+          />
+        ),
+        title: "Posts",
+        callBack: handlePostsClick,
+      },
+    ]);
   }, []);
 
   return (
